6bf3c27ea6f15cd2c80875c88b8cf2b96aa3dc14
Waled Karim <waledkarim445@gmail.com>
2026-03-03 16:57:59 +0600

    chore: project setup and tailwindcss setup

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Epilogue, Red_Hat_Display } from "next/font/google";
+import "./globals.css";
+
+const epilogue = Epilogue({
+  variable: "--font-epilogue",
+  weight: ["400"],
+  style: "normal",
+  subsets: ["latin"],
+});
+
+const redHatDisplay = Red_Hat_Display({
+  variable: "--font-red-hat-display",
+  weight: ["600"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "WaledJobs",
+  description:
+    "A simple job board application created as a task of QTec Solution Limited.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${epilogue.variable} ${redHatDisplay.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
