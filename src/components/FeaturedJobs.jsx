4db8ab1ba75bd513b44523b85071907812178c52
Waled Karim <waledkarim445@gmail.com>
2026-03-06 03:46:37 +0600

    fix: fix featured jobs cards layout

--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -11,7 +11,10 @@ export default async function FeaturedJobs() {
   return (
     <div className="featuredjobs-grid mb-6 lg:mb-0 overflow-x-auto lg:overflow-x-hidden">
       {jobs.slice(0, 9).map((job, i) => (
-        <div key={i} className="p-6 w-71.5 border border-[#D6DDEB]">
+        <div
+          key={i}
+          className="grid grid-rows-subgrid row-span-4 p-6 w-71.5 border border-[#D6DDEB] overflow-hidden"
+        >
           <div className="flex items-center justify-between mb-4">
             <Image src={generateCompanyImage()} width={40} height={40} />
             <span className="border border-[#4640DE] py-1 px-3 text-[#4640DE] text-[16px]">
@@ -22,7 +25,9 @@ export default async function FeaturedJobs() {
             <p className="font-semibold text-[18px]">{job.title}</p>
             <p className="text-[16px]">{job.location}</p>
           </div>
-          <p className="text-[16px] mb-4">{job.description}</p>
+          <p className="text-[16px] mb-4 text-nowrap truncate">
+            {job.description}
+          </p>
           <div className="flex items-center gap-x-2">
             <div className="rounded-[100vw] font-semibold text-[14px] w-fit text-nowrap py-1 px-4 bg-[#EB85331A] text-[#FFB836]">
               {job.category}
